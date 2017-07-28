110965a7c23de1a8a88cfcc43236b99eda61380b
Emmanuel Pot <emmanuel.pot@gmail.com>
2017-07-28 13:52:06 +0200

    Cleanup

--- app/controllers/SignUpController.scala
+++ app/controllers/SignUpController.scala
@@ -43,37 +43,38 @@ class SignUpController @Inject() (
    * @return The result to display.
    */
   def submit = Action.async(parse.json) { implicit request =>
-    request.body.validate[SignUpForm.Data].map { data =>
-      val loginInfo = LoginInfo(CredentialsProvider.ID, data.email)
-      userService.retrieve(loginInfo).flatMap {
-        case Some(_) =>
-          Future.successful(BadRequest(Json.obj("message" -> Messages("user.exists"))))
-        case None =>
-          val authInfo = passwordHasher.hash(data.password)
-          val user = User(
-            userID = UUID.randomUUID(),
-            loginInfo = loginInfo,
-            firstName = Some(data.firstName),
-            lastName = Some(data.lastName),
-            fullName = Some(data.firstName + " " + data.lastName),
-            email = Some(data.email),
-            avatarURL = None
-          )
-          for {
-            avatar <- avatarService.retrieveURL(data.email)
-            user <- userService.save(user.copy(avatarURL = avatar))
-            _ <- authInfoRepository.add(loginInfo, authInfo)
-            authenticator <- silhouette.env.authenticatorService.create(loginInfo)
-            token <- silhouette.env.authenticatorService.init(authenticator)
-          } yield {
-            silhouette.env.eventBus.publish(SignUpEvent(user, request))
-            silhouette.env.eventBus.publish(LoginEvent(user, request))
-            Ok(Json.obj("token" -> token))
-          }
+    SignUpForm.form.bindFromRequest.fold(
+      _ =>
+        Future.successful(BadRequest(Json.obj("message" -> Messages("invalid.data")))),
+      data => {
+        val loginInfo = LoginInfo(CredentialsProvider.ID, data.email)
+        userService.retrieve(loginInfo).flatMap {
+          case Some(_) =>
+            Future.successful(BadRequest(Json.obj("message" -> Messages("user.exists"))))
+          case None =>
+            val authInfo = passwordHasher.hash(data.password)
+            val user = User(
+              userID = UUID.randomUUID(),
+              loginInfo = loginInfo,
+              firstName = Some(data.firstName),
+              lastName = Some(data.lastName),
+              fullName = Some(data.firstName + " " + data.lastName),
+              email = Some(data.email),
+              avatarURL = None
+            )
+            for {
+              avatar <- avatarService.retrieveURL(data.email)
+              user <- userService.save(user.copy(avatarURL = avatar))
+              _ <- authInfoRepository.add(loginInfo, authInfo)
+              authenticator <- silhouette.env.authenticatorService.create(loginInfo)
+              token <- silhouette.env.authenticatorService.init(authenticator)
+            } yield {
+              silhouette.env.eventBus.publish(SignUpEvent(user, request))
+              silhouette.env.eventBus.publish(LoginEvent(user, request))
+              Ok(Json.obj("token" -> token))
+            }
+        }
       }
-    }.recoverTotal {
-      case _ =>
-        Future.successful(Unauthorized(Json.obj("message" -> Messages("invalid.data"))))
-    }
+    )
   }
 }
